067acd32fad84d445f6cc49dcee4f54ba13a6043
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2022-12-07 21:58:55 +0300

    fix: change API_URL

--- js/const.js
+++ js/const.js
@@ -1,2 +1,5 @@
-export const API_URL = "https://hissing-endurable-antlion.glitch.me";
+// export const API_URL = "https://hissing-endurable-antlion.glitch.me";
+// export const API_URL = "https://api-your-meal.vercel.app";
+
+export const API_URL = "https://flax-great-burst.glitch.me";
 export const PREFIX_PRODUCT = "/api/product";
